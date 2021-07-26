fcb22e72e13ac8a308777942cfdfc8911ca3113b
leo-holanda <leo.uldholanda@gmail.com>
2021-07-26 17:44:58 -0300

    Add recipes model and recipes list initial template

--- src/app/components/Recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/Recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+    'Torta de frango',
+    'A melhor torta de frango',
+    'https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg'
+    )
+  ]
 
   constructor() { }
 
