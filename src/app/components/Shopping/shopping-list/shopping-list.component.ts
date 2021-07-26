534d080d666b21f4fdec6b732caea1815f24b110
leo-holanda <leo.uldholanda@gmail.com>
2021-07-26 18:12:57 -0300

    Add ingredient model, shopping list and shopping list edit initial template

--- src/app/components/Shopping/shopping-list/shopping-list.component.ts
+++ src/app/components/Shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Frango', 1),
+    new Ingredient('Farinha', 5),
+    new Ingredient('Queijo', 3)
+  ];
 
   constructor() { }
 
