979a187fe0fef38f2442ee94006cacc76610b25b
leo-holanda <leo.uldholanda@gmail.com>
2021-08-02 15:12:26 -0300

    Add proper recipes item component code from recipes list

--- src/app/components/Recipes/recipes-list/recipes-list.component.ts
+++ src/app/components/Recipes/recipes-list/recipes-list.component.ts
@@ -12,6 +12,11 @@ export class RecipesListComponent implements OnInit {
     'Torta de frango',
     'A melhor torta de frango',
     'https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg'
+    ),
+    new Recipe(
+      'Torta de chocolate',
+      'A melhor torta de chocolate',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chocolate-pie-1599233993.jpg?crop=0.839xw:0.839xh;0.0856xw,0&resize=640:*'
     )
   ]
 
