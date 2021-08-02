f8d387a04b02379a1f1970758c9ff976b204251a
leo-holanda <leo.uldholanda@gmail.com>
2021-08-02 14:57:01 -0300

    Add initial navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes-project';
+  currentSection = 'recipes'
+
+  onSetCurrentSection(newSection){
+    this.currentSection = newSection
+  }
 }
--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true
+  @Output() currentSection = new EventEmitter<String>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setCurrentSection(newSection: string){
+    this.currentSection.emit(newSection)
+  }
 }
