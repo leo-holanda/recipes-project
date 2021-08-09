bbb89c3ae7ac50ba4c69379cec96fb5f9d86dca2
leo-holanda <leo.uldholanda@gmail.com>
2021-08-09 17:22:39 -0300

    Add ingredients in recipes and show them in template

--- src/app/services/Recipes/recipes.service.ts
+++ src/app/services/Recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from 'src/app/components/Recipes/recipes.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +14,24 @@ export class RecipesService {
     new Recipe(
     'Torta de frango',
     'A melhor torta de frango',
-    'https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg'
+    'https://food-images.files.bbci.co.uk/food/recipes/chicken_pie_24044_16x9.jpg',
+    [
+      new Ingredient('Frango', 1),
+      new Ingredient('Farinha de trigo', 5),
+      new Ingredient('Queijo', 3),
+      new Ingredient('Pimenta do reino', 2)
+    ]
     ),
     new Recipe(
       'Torta de chocolate',
       'A melhor torta de chocolate',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chocolate-pie-1599233993.jpg?crop=0.839xw:0.839xh;0.0856xw,0&resize=640:*'
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chocolate-pie-1599233993.jpg?crop=0.839xw:0.839xh;0.0856xw,0&resize=640:*',
+      [
+        new Ingredient('Chocolate', 10),
+        new Ingredient('Farinha de trigo', 5),
+        new Ingredient('Baunilha', 3),
+        new Ingredient('Brigadeiro', 5)
+      ]
     )
   ]
 
